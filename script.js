8a1b6abbf45edeef295ec5a7ac6b84a1f1144b88
Vincent <vmgboyles@gmail.com>
2026-09-03 20:20:52 +0800

    created new Gameboard function

---
+++ script.js
@@ -0,0 +1,12 @@
+function Gameboard() {
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+            board[i].push()
+        }
+    }
+}
